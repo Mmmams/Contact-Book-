685f5787e027d316c411d3322829beb095bc54b2
Сергей Мамыкин <bymamykin@gmail.com>
2021-01-28 15:55:09 +0300

    a

--- src/views/contactInfo.vue
+++ src/views/contactInfo.vue
@@ -4,17 +4,6 @@
         <div class="box_info">
             <div v-for="item in contacts[contactId]" :key="item.id"><h2>{{item}}</h2></div>
         </div>
-        <div class='box_inputs'>
-            <input type="text" class="form-control" v-model="fieldName" :placeholder="fieldNamePlaceholder"/>
-            <input type="text" class="form-control" v-model="fieldValue" :placeholder="fieldValuePlaceholder"/>
-        </div>
-        <div class="box_btn">
-            <button class="btn btn-primary" 
-            @click='contacts[contactId].fieldName = fieldValue;
-            fieldName = "";
-            fieldValue = "";
-            '>Добавить поле</button>
-        </div>
     </div>
     
 </div>
@@ -26,15 +15,6 @@ import {mapState} from 'vuex'
 
 export default {
     name: 'contactInfo',
-    data() {
-        return {
-            fieldName: '',
-            fieldValue: '',
-            fieldNamePlaceholder: 'email',
-            fieldValuePlaceholder: 'test@test.com',
-            newRecord: null
-        }
-    },
     props: {
         contactId: {
             requied: true
